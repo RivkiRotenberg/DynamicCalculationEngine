e94e808e68874db0d017fe1109dda9c593c758b4
rivki0259mby <39329397731@mby.co.il>
2026-04-16 18:12:53 +0300

    Add: insert sample formulas and conditional logic into t_targil

---
+++ Scripts/insert formulas to targil table.sql
@@ -0,0 +1,10 @@
+INSERT INTO t_targil(targil,tnai,targil_false)
+VALUES
+
+--Simple formulas
+('a + b - 5 ',NULL,NULL),
+('a * b', NULL,NULL),
+
+--Complex formulas
+('(a + b) * 8',NULL , NULL),
+('(sqrt(c*c + d*d)',NULL,NULL)
