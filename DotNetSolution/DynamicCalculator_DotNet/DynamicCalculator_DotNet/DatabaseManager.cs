ad5e7aaa2929564c4a94c152030c8042ad97bca2
rivki0259mby <39329397731@mby.co.il>
2026-04-16 18:04:01 +0300

    Feat: implement DatabaseManager with BulkCopy for 1M records seeding

---
+++ DotNetSolution/DynamicCalculator_DotNet/DynamicCalculator_DotNet/DatabaseManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DynamicCalculator_DotNet
+{
+    internal class DatabaseManager
+    {
+        private string _connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        //fill a million records
+        public void SeedDatabase()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("a", typeof(double));
+            table.Columns.Add("b", typeof(double));
+            table.Columns.Add("c", typeof(double));
+            table.Columns.Add("d", typeof(double));
+
+            Random rand = new Random();
+            for (int i = 0; i<1000000; i++)
+            {
+                float valA = (float)(rand.NextDouble() * 100);
+                float valB = (float)(rand.NextDouble() * 100);
+                float valC = (float)(rand.NextDouble() * 100);
+                float valD = (float)(rand.NextDouble() * 100);
+                table.Rows.Add(valA, valB, valC, valD);
+
+
+            }
+            using (SqlBulkCopy bulkCopy = new SqlBulkCopy(_connectionString))
+            {
+                bulkCopy.DestinationTableName = "t_data";
+
+                bulkCopy.ColumnMappings.Add("a", "a");
+                bulkCopy.ColumnMappings.Add("b", "b");
+                bulkCopy.ColumnMappings.Add("c", "c");
+                bulkCopy.ColumnMappings.Add("d", "d");
+
+                bulkCopy.WriteToServer(table);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ DotNetSolution/DynamicCalculator_DotNet/DynamicCalculator_DotNet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicCalculator_DotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = @"Server = DESKTOP-FM7OT9G; Database = DynamicDB; Trusted_Connection = True;TrustServerCertificate=True;";
+
+            DatabaseManager dbManager = new DatabaseManager(connString);
+
+            Console.WriteLine("---Start filling in data---");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            try
+            {
+                dbManager.SeedDatabase();
+                sw.Stop();
+                Console.WriteLine($"A million records were entered in {sw.Elapsed.TotalSeconds:F2} seconds successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error : " + ex.Message);
+            }
+            Console.WriteLine("לחצי על מקש כלשהו לסיום...");
+            Console.ReadKey();
+        }
+    }
+}
