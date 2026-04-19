8df9ac7750e79d971c412efa2bf962431a40689c
rivki0259mby <39329397731@mby.co.il>
2026-04-19 03:34:16 +0300

    feat(sql): add conditional logic support to dynamic formulas SP

--- SQLSolution/Calculate_Formulas_Procedure.sql
+++ SQLSolution/Calculate_Formulas_Procedure.sql
@@ -1,4 +1,4 @@
-USE DynamicDB
+﻿USE DynamicDB
 GO
 
 IF OBJECT_ID('sp_CalcultateDynamicalsFormulas','P') IS NOT NULL
@@ -12,32 +12,47 @@ BEGIN
 
 	DECLARE @FormulaId INT;
     DECLARE @FormulaStr NVARCHAR(MAX);
+	DECLARE @Condition NVARCHAR(MAX);
+	DECLARE @FormulaFalse NVARCHAR(MAX);
     DECLARE @SQL NVARCHAR(MAX);
     DECLARE @StartTime DATETIME2;
     DECLARE @EndTime DATETIME2;
 
-	IF CURSOR_STATUS('global','formula_cursor')>=1
-	BEGIN
-		DEALLOCATE formula_cursor;
-	END
+	
 
 	DECLARE formula_cursor CURSOR LOCAL FOR
-	SELECT CAST(targil_id AS INT), CAST(targil AS NVARCHAR(MAX))
+	SELECT targil_id , targil ,tnai,targil_false
 	FROM t_targil
 
 	OPEN formula_cursor;
-
-	FETCH NEXT FROM formula_cursor INTO @FormulaId,@FormulaStr;
+	FETCH NEXT FROM formula_cursor INTO @FormulaId,@FormulaStr,@Condition,@FormulaFalse;
 
 	WHILE @@FETCH_STATUS = 0
 	BEGIN 
 		SET @StartTime = SYSDATETIME();
 
+		
+			
+
+		SET @Condition = REPLACE(@Condition, '==', '=');
+
 		DECLARE @IdStr NVARCHAR(10)= CAST(@FormulaId AS NVARCHAR(10));
 
-		SET @SQL = N'INSERT INTO t_results (data_id,targil_id,method,result) ' +
-		           N'SELECT data_id, ' + @IdStr + N', ''DB'', (' +@FormulaStr+ N') '+
-				   N'FROM t_data';
+		IF (LEN(@Condition)>0 AND UPPER(@Condition) <> 'NULL')
+		BEGIN
+			SET @SQL = N'INSERT INTO t_results (data_id,targil_id,method,result) '+
+					   N'SELECT data_id, ' +@IdStr + N', ''DB'', '+
+					           N'CASE WHEN ('+@Condition+ N') THEN CAST((' + @FormulaStr + N') AS FLOAT) ' +
+							   N'ELSE CAST((' + ISNULL(@FormulaFalse, '0' ) + N') AS FLOAT) END ' +
+							   N'FROM t_data';
+		END
+		ELSE
+		BEGIN
+			SET @SQL = N'INSERT INTO t_results (data_id,targil_id,method,result) ' +
+					   N'SELECT data_id, ' + @IdStr + N', ''DB'', CAST((' +@FormulaStr+ N') AS FLOAT) '+
+				       N'FROM t_data';
+		END
+		PRINT 'PROCESSING ID' +@IdStr+'whit' +@SQL;
 
 		BEGIN TRY
 			EXEC sp_executesql @SQL;
@@ -51,7 +66,7 @@ BEGIN
 		INSERT INTO t_log (targil_id,method,run_time)
 		VALUES (@FormulaId,'DB',CAST(DATEDIFF(MILLISECOND,@StartTime,@EndTime) AS FLOAT) /1000.0);
 
-		FETCH NEXT FROM formula_cursor INTO @FormulaId,@FormulaStr;
+		FETCH NEXT FROM formula_cursor INTO @FormulaId,@FormulaStr,@Condition,@FormulaFalse;
 	END;
 
 	CLOSE formula_cursor;
@@ -60,4 +75,5 @@ BEGIN
 	PRINT' THE PROCESS FINISHED SUCCESSFULLY';
 END;
 GO
+
 
