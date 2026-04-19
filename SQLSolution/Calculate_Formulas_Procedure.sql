f09419534ef877a62962fcf86973837f576498ab
rivki0259mby <39329397731@mby.co.il>
2026-04-19 05:44:13 +0300

    Adding comments and explanations to the code on SQL

--- SQLSolution/Calculate_Formulas_Procedure.sql
+++ SQLSolution/Calculate_Formulas_Procedure.sql
@@ -9,7 +9,7 @@ ALTER PROCEDURE sp_CalculateDynamicalsFormulas
 AS
 BEGIN 
 	SET NOCOUNT ON;
-
+	--הגדרת משתנים
 	DECLARE @FormulaId INT;
     DECLARE @FormulaStr NVARCHAR(MAX);
 	DECLARE @Condition NVARCHAR(MAX);
@@ -19,7 +19,7 @@ BEGIN
     DECLARE @EndTime DATETIME2;
 
 	
-
+	--הגדרת מצביע לטבלת התרגילים כדי לחשב כל נוסחה
 	DECLARE formula_cursor CURSOR LOCAL FOR
 	SELECT targil_id , targil ,tnai,targil_false
 	FROM t_targil
@@ -27,6 +27,7 @@ BEGIN
 	OPEN formula_cursor;
 	FETCH NEXT FROM formula_cursor INTO @FormulaId,@FormulaStr,@Condition,@FormulaFalse;
 
+	--כל עוד השליפה מצליחה
 	WHILE @@FETCH_STATUS = 0
 	BEGIN 
 		SET @StartTime = SYSDATETIME();
@@ -37,9 +38,10 @@ BEGIN
 		SET @Condition = REPLACE(@Condition, '==', '=');
 
 		DECLARE @IdStr NVARCHAR(10)= CAST(@FormulaId AS NVARCHAR(10));
-
+		--בדיקה אם קיימת נוסחת תנאי
 		IF (LEN(@Condition)>0 AND UPPER(@Condition) <> 'NULL')
 		BEGIN
+			--t_results חישבות שאילתה זמינית לחישוב התוצאות והכנסה שלהן לטבלה 
 			SET @SQL = N'INSERT INTO t_results (data_id,targil_id,method,result) '+
 					   N'SELECT data_id, ' +@IdStr + N', ''DB'', '+
 					           N'CASE WHEN ('+@Condition+ N') THEN CAST((' + @FormulaStr + N') AS FLOAT) ' +
@@ -48,12 +50,13 @@ BEGIN
 		END
 		ELSE
 		BEGIN
+			-- t_results  במקרה ואין נוסחת תנאי חישוב הנוסחה הרגילה והכנסת התוצאה לטבלה   
 			SET @SQL = N'INSERT INTO t_results (data_id,targil_id,method,result) ' +
 					   N'SELECT data_id, ' + @IdStr + N', ''DB'', CAST((' +@FormulaStr+ N') AS FLOAT) '+
 				       N'FROM t_data';
 		END
 		PRINT 'PROCESSING ID' +@IdStr+'whit' +@SQL;
-
+		--נסיון הרצת השאילתה הזמנית
 		BEGIN TRY
 			EXEC sp_executesql @SQL;
 		END TRY
@@ -62,7 +65,7 @@ BEGIN
 		END CATCH
 
 		SET @EndTime = SYSDATETIME();
-
+		--t_log הכנסת זמן הריצה של הנוסחה לטבלת 
 		INSERT INTO t_log (targil_id,method,run_time)
 		VALUES (@FormulaId,'DB',CAST(DATEDIFF(MILLISECOND,@StartTime,@EndTime) AS FLOAT) /1000.0);
 
--- SQLSolution/SeedData.sql
+++
@@ -1,8 +0,0 @@
-DECLARE @i INT = 1;
-WHILE @i <= 5
-BEGIN
-	INSERT INTO t_data (a,b,c,d)
-	VALUES (RAND()*10,RAND()*10,RAND()*10,RAND()*10);
-	SET @i = @i +1;
-END;
-
